870421cd9f60623ee95e26afa92126a75e5b1399
Romain <ro.buisson@gmail.com>
2016-02-20 18:03:28 +0700

    MRSMED-7: Add unit tests

---
+++ omod/src/test/webapp/resources/scripts/medication.js
@@ -0,0 +1,7 @@
+describe("Medication functions", function() {
+
+	// Simple useless test just to put the framework in place
+    it("should pass: expect true to be true", function() {
+    	expect(true).toBe(true);
+    });
+});
