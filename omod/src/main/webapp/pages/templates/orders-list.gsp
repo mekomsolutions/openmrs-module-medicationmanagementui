f7fc8b35fc86d46c76e5761af7a955336397c4d8
Romain <ro.buisson@gmail.com>
2016-04-01 17:35:41 +0700

    MRSMED-9: Improve the way the allDrugOrders array is filtered by visit 	modified:   omod/src/main/webapp/pages/templates/orders-list.gsp 	modified:   omod/src/main/webapp/resources/scripts/medicationManagementUI.js

--- omod/src/main/webapp/pages/templates/orders-list.gsp
+++ omod/src/main/webapp/pages/templates/orders-list.gsp
@@ -1,7 +1,9 @@
 <ul>
-	<h3 ng-show="visit">Orders for visit {{visit.display}}</h3>
-	<h3 ng-hide="visit">Orders</h3>
-	<li ng-repeat="order in drugOrders | orderBy:'dateActivated':true">
+	<h3 ng-show="visit">Orders (for visit {{visit.display}})</h3>
+	<h3 ng-hide="visit">Orders (for any visit)</h3>
+	<a ng-click="loadExistingOrders()">Refresh</a>
+
+	<li style="margin-top: 20px;" ng-repeat="order in allDrugOrders | filter:visit.uuid | orderBy:'dateActivated':true">
 		<div ng-hide="visit" style="">* Visit: {{order.visit.display}}</div>
 		<div style="color:red;"><span style="margin-right: 20px;">* {{order.orderNumber}} </span><span> {{order.dateActivated | date:'dd/MM/yyyy @ h:mma'}}</span></div>
 		<div style="color:green; border-bottom:solid 1px">* {{order.display}}</div>
--- omod/src/main/webapp/resources/scripts/medicationManagementUI.js
+++ omod/src/main/webapp/resources/scripts/medicationManagementUI.js
@@ -17,10 +17,14 @@ angular.module('MedicationManagementUI', ['orderService', 'encounterService'])
 	};
 })
 
-.controller('MMUIOrderListCtrl', ['$scope', '$filter', 'OrderService',
-	function($scope, $filter, OrderService) {
+.controller('MMUIOrderListCtrl', ['$scope', '$filter', 'OrderService', 'Encounter',
+	function($scope, $filter, OrderService, Encounter) {
+
 
 		$scope.loadExistingOrders = function() {
+
+		$scope.allDrugOrders = [];
+		$scope.visit = OpenMRS.drugOrdersConfig.visit;
 
 			OrderService.getOrders({
 				t: 'drugorder',
@@ -28,50 +32,23 @@ angular.module('MedicationManagementUI', ['orderService', 'encounterService'])
 				patient: OpenMRS.drugOrdersConfig.patient.uuid,
 				careSetting: '6f0c9a92-6f24-11e3-af88-005056821db0'
 			}).then(function(results) {
-				$scope.activeDrugOrders = results;
-				if (typeof OpenMRS.drugOrdersConfig.visit == "undefined") {
-					$scope.drugOrders = $filter('filterByVisit')($scope, $scope.activeDrugOrders);
-				} else {
-					$scope.drugOrders = $filter('filterByVisit')($scope, $scope.activeDrugOrders, OpenMRS.drugOrdersConfig.visit.uuid);
-				}
+				$scope.allDrugOrders = results;
+				
+				$scope.allDrugOrders.forEach(function(currentOrder, index) {
+					Encounter.get({
+						uuid: currentOrder.encounter.uuid
+					}).$promise.then(function(encounter) {
+						if (encounter.visit != null) {
+							currentOrder.visit = encounter.visit;
+						} else {
+							currentOrder.visit = {"uuid":""};
+						}
+					});
+				});	
 			});
 		};
 
-		$scope.loadExistingOrders();
-		$scope.visit = OpenMRS.drugOrdersConfig.visit;
-
+		$scope.loadExistingOrders();	
+		
 	}
 	])
-
-.filter('filterByVisit', ['Encounter', function(Encounter) {
-	return function($scope, orders, visit) {
-		var encounterType = config.orderEncounterType;
-
-		var encounters = [];
-		var visits = {};
-		var filtered = [];
-
-		if (orders.length > 0) {
-			orders.forEach(function(currentOrder, index) {
-				Encounter.get({
-					uuid: currentOrder.encounter.uuid
-				}).$promise.then(function(encounter) {
-					if (encounter.visit != null) {
-						currentOrder.visit = encounter.visit;
-					}
-
-					if (visit != null) {
-						if (currentOrder.visit != null && currentOrder.visit.uuid == visit) {
-							filtered.push(currentOrder);
-						}
-					} else {
-						filtered.push(currentOrder);
-					}
-				});
-
-			});
-		}
-
-		return filtered;
-	}
-}])
